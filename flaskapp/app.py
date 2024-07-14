4794c530206d4e146a620249782741eff51d218b
KensukeKawai <kawai.tech.kk39@gmail.com>
2024-07-14 15:59:47 +0900

    request func

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -6,9 +6,26 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 db = SQLAlchemy(app)
 
-@app.route('/')
+# @app.route('/')
+# def hello():
+#     return 'Hello, Flask!'
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return 'Hello, Flask!'
+    name = request.form.get('name', 'World')
+    html_form = """
+        <html>
+            <body>
+                <form method="post">
+                    <label for="name">Enter your name:</label>
+                    <input type="text" id="name" name="name">
+                    <input type="submit" value="Submit">
+                </form>
+                <h1>Hello, {}!</h1>
+            </body>
+        </html>
+    """.format(name)
+    return render_template_string(html_form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
